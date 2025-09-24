501beabf2d178cc6ca9eeea497626c542ffd2fd9
Peter van der Wal <peter.vanderwal@youweagency.com>
2025-09-24 16:48:27 +0200

    fix: exclude files from installed Symfony Bundles in PhpCsFixer
    Error: PhpCsFixer complained about files in the public/bundles folder, but those are symlinks to Symfony bundles in the vendor/ folder
    Resolved with: adding public/bundles to the exclude list

--- config/pimcore/php-cs-fixer/excludes.php
+++ config/pimcore/php-cs-fixer/excludes.php
@@ -6,6 +6,7 @@ return [
     // Symfony files
     'bin/console',
     'public/index.php',
+    'public/bundles',
     // Full var folder, contains all kind of auto-generated files (e.g. cache, generated Pimcore DataObject classes)
     'var',
     // Configuration written by Pimcore (which is written either to var/ or to config/pimcore/ depending on settings)
